264032749c8e484553ae0300a4a8d21755e32423
Matteo Hertel <info@matteohertel.com>
2022-12-15 18:24:20 +0000

    fix(types):forgot how to typescript

--- packages/ui/Button/component.tsx
+++ packages/ui/Button/component.tsx
@@ -4,7 +4,7 @@ import classNames from "classnames";
 
 import styles from "./styles.module.css";
 
-export const Button = ({ fancy }: { fancy?: bool }) => {
+export const Button = ({ fancy }: { fancy?: boolean }) => {
   const className = classNames({ [styles.fancyButton]: fancy });
   return <button className={className}>Boop</button>;
 };
