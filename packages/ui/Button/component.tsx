fb6e5cda381f3fd989f213ecd127094a4d8267a4
Matteo Hertel <info@matteohertel.com>
2022-12-15 18:17:39 +0000

    feat(monorepo):add transpile module to allow css module in monorepo

---
+++ packages/ui/Button/component.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+import classNames from "classnames";
+
+import styles from "./styles.module.css";
+
+export const Button = ({ fancy }: { fancy?: bool }) => {
+  const className = classNames({ [styles.fancyButton]: fancy });
+  return <button className={className}>Boop</button>;
+};
